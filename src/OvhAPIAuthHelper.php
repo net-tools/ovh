05fee5b822c5863956a8ec9bb5c2bb8e6b86c3a2
Pierre <dev@nettools.ovh>
2021-03-12 15:09:01 +0100

    first commit as nettools/ovh

---
+++ src/OvhAPIAuthHelper.php
@@ -0,0 +1,67 @@
+<?php
+
+// namespace
+namespace Nettools\Ovh;
+
+
+
+
+// helper class for OVH api authentication
+class OvhAPIAuthHelper{
+	
+	/**
+	 * Get a consumer key and a validation url ; ovh account should be link to consumer key by visiting the url
+	 *
+	 * @param string $appKey
+	 * @param string $appSecret
+	 * @param string $redirection Url of application to redirect to after successful ovh account linking
+	 * @param string[] $verbs HTTP verbs array (GET, POST, etc.)
+	 * @param string $endpoint 
+	 *
+	 * For information, this verbs array is later transformed to the following rights structure
+	 *	[
+	 * 		{
+	 * 			"method": "GET",
+	 *			"path": "/*"
+	 *		}
+	 *	]
+	 */
+	static function authorize($appKey, $appSecret, $redirection, array $verbs, $endpoint = 'ovh-eu')
+	{
+		session_start();
+		$_SESSION['consumer_key'] = NULL;
+		
+		
+		// prepare rights
+		$rights = array();
+		foreach ( $verbs as $grant )
+			$rights[] = (object) [
+								'method'    => $grant,
+								'path'      => "/*"
+							];
+		
+				
+		// Get credentials
+		$conn = new \Ovh\Api($appKey, $appSecret, $endpoint);
+		$credentials = $conn->requestCredentials($rights, $redirection);
+		
+		// store consumer key in session, to be later retrieved back from ovh url
+		$_SESSION['consumer_key'] = $credentials["consumerKey"];
+		return $credentials["validationUrl"];
+	}
+	
+	
+	
+	/**
+	 * Get the consumer key after successfull authentication and ovh account linking
+	 *
+	 * @return string
+	 */
+	static function authorized()
+	{
+		session_start();
+		return $_SESSION['consumer_key'];
+	}
+}
+
+?>
---
+++ src/OvhAPIDbHelper.php
@@ -0,0 +1,79 @@
+<?php
+
+// namespace
+namespace Nettools\Ovh;
+
+
+
+
+// Helper class to dump OVH database
+class OvhAPIDbHelper{
+	
+	/** 
+	 * Dump a database
+	 *
+	 * @param \Ovh\Api $api
+	 * @param string $service OVH service (usually, the web domain)
+	 * @param string $db database name (xxx.mysql.db)
+	 * @return string Returns a task id or NULL if an error occurs
+	 */
+	static function databaseDump(\Ovh\Api $api, $service, $db)
+	{
+		// ask for a "now" dump
+		$task = $api->post("/hosting/web/$service/database/$db/dump", 
+							[
+								'date' => 'now'
+							]);
+
+		// dump to be created 
+		if ( is_array($task) && array_key_exists('objectId', $task) )
+			return $task['objectId'];
+		else
+			return NULL;
+	}
+	
+	
+	
+	
+	/** 
+	 * Check a database dump status ; if available, download it and halt script
+	 *
+	 * @param \Ovh\Api $api
+	 * @param string $service OVH service (usually, the web domain)
+	 * @param string $db database name (xxx.mysql.db)
+	 * @param string $id Task id returned by `databaseDump`
+	 * @return string Returns a status string if dump is not ready, or redirects to download url
+	 */
+	static function databaseDumpStatus(\Ovh\Api $api, $service, $db, $id)
+	{
+		$task = $api->get("/hosting/web/$service/database/$db/dump/$id");
+
+		if ( is_array($task) && array_key_exists('status', $task) )
+			if ( $task['status'] == 'created' )
+			{
+				header('Location: ' . $task['url']);
+				die();
+			}
+			else
+				return $task['status'];
+		else
+			return 'unknown';
+	}
+	
+		
+	
+	/** 
+	 * Remove a database dump
+	 *
+	 * @param \Ovh\Api $api
+	 * @param string $service OVH service (usually, the web domain)
+	 * @param string $db database name (xxx.mysql.db)
+	 * @param string $id Task id returned by `databaseDump`
+	 */
+	static function databaseDumpDelete(\Ovh\Api $api, $service, $db, $id)
+	{
+		$api->delete("/hosting/web/$service/database/$db/dump/$id");
+	}
+}
+
+?>
